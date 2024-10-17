2f0ff99200040630cc2bbfc97d3348bc24df7c26
Luis Garcia <garcia.rivera.luis.108@gmail.com>
2024-10-17 14:51:47 -0700

    empezamospoo

---
+++ src/poo/Usocoche.java
@@ -0,0 +1,18 @@
+package poo;
+
+public class Usocoche {
+    public static void main(String[] args) {
+        //crear objetos de la clase cohe(INSTANCIAR )
+         Coche Toyota=new Coche();
+         //darle parametros
+        System.out.println(Toyota.ruedas);
+
+
+
+
+
+
+
+
+    }
+}
