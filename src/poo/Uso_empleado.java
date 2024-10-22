85601d52199ad1c60bbe99b8424ff5bd17d40533
Luis Garcia <garcia.rivera.luis.108@gmail.com>
2024-10-22 15:21:09 -0700

    herencia2

--- src/poo/Uso_empleado.java
+++ src/poo/Uso_empleado.java
@@ -1,5 +1,7 @@
 package poo;
 
+import javax.swing.*;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.GregorianCalendar;
 public class Uso_empleado {
@@ -7,23 +9,55 @@ public class Uso_empleado {
 
         Empleado empleado1=new Empleado("Luis",1000.00, 2023,2,23);
         Empleado empleado2=new Empleado("Luisaaa",1050.00, 2024,5,27);
+        Jefatura jefaso=new Jefatura("Pedro", 2000.00, 2022, 10, 15);
 
-        empleado1.set_subeSueldo(5);
-        empleado2.set_subeSueldo(2);
-        System.out.println("nombre: "+empleado1.getNombre()+"  sueldo: "+empleado1.getSueldo()+"  se contrato en: "+empleado1.getAltacontrato());
+        jefaso.setIncentivo(1000.25);
+
+        Empleado [] misempleados=new Empleado[4];
+        misempleados[0]=empleado1;
+        misempleados[1]=empleado2;
+        misempleados[2]=jefaso;
+
+        /*a este jefe no se le puede aplicar el bono porque esta almasenado en misempleados
+        asi que lo lo toma como si fuera de la clase "Empleado"
+        para poder aplicarlo debemos hacer un CASTING(refundicion)*/
+
+        misempleados[3]=new Jefatura("Pablito", 2000.00, 2022, 10, 15);
+
+        //sintaxis= clase nuevo_nombre=(clase)valor que deseas hacer casting;
+        Jefatura jefaso2=(Jefatura) misempleados[3];
+
+        //ahora si nos permite aplicar el bono
+
+
+
+
+
+
+ for (Empleado e:misempleados){
+     System.out.println("nombre: "+e.getNombre()+" sueldo: "+e.getSueldo());
+ /*aqui se ejemplifica el polimorfismo porque las clases empleado y jefaso
+    aplican el metodo get sueldo de diferente manera  */
+
+ }
+
+
+
+
+
 
 
 
     }
 }
-class Empleado{
+class Empleado {
     public Empleado(String nom, double sue, int year, int month, int day) {
 
-        nombre=nom;
-        sueldo=sue;
+        nombre = nom;
+        sueldo = sue;
         //clase que construye una fecha
-        GregorianCalendar calendar=new GregorianCalendar(year, month, day);
-        altacontrato=calendar.getTime();
+        GregorianCalendar calendar = new GregorianCalendar(year, month, day);
+        altacontrato = calendar.getTime();
 
 
     }
@@ -41,40 +75,44 @@ class Empleado{
     }
 
     public void set_subeSueldo(double porcentaje) {
-        double aumento = sueldo*porcentaje/100;
-        sueldo+=aumento;
+        double aumento = sueldo * porcentaje / 100;
+        sueldo += aumento;
 
 
     }
 
-    private  String nombre;
+    private String nombre;
     private double sueldo;
     private Date altacontrato;
+    private static int IdSiguiente;
+    private int Id;
+}
+
+class Jefatura extends Empleado{
+
+    public Jefatura(String nom,double sue,int year, int month, int day){
+        super(nom, sue, year, month, day);
 
 
+    }
 
 
+    public void setIncentivo(double incen) {
+        Incentivo = incen;
+    }
+    @Override
+    public double getSueldo() {
+        if (Incentivo==0){
 
-
+           Incentivo= Double.parseDouble(JOptionPane.showInputDialog("cuanto insentivo"));
+        }
+        double sueldojefe=super.getSueldo();
+        return sueldojefe+Incentivo;
+    }
+    private double Incentivo=0;
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
